c0f4ae82eff4ad823b0f7a64ccac3d711ad89c78
Ömer Serfice <srfcomr@gmail.com>
2025-02-27 02:00:33 +0300

    Add project files.

---
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services,IConfiguration configuration)
+        {
+			services.AddDbContext<AppDbContext>(options =>
+			{
+				var connectionStrings = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+				options.UseSqlServer(connectionStrings!.SqlServer,sqlServerOptionsAction =>
+				{
+					sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
+				});
+			});
+
+			return services;
+		}
+    }
+}
